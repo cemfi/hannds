e31b61128fcad225b4399ca5af9c75af32c6bf3b
Simon Waloschek <simon.waloschek@posteo.de>
2017-10-24 20:33:15 +0200

    Resolved multiple quirks. All midi files are now loaded in a single tensor.

--- hannds.py
+++ hannds.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 @jit
-def convert(path, ms_window=20):
+def convert(path, ms_window=20, overwrite=False):
     if os.path.isfile(path):
         midi_files = [path]
     else:
@@ -24,24 +24,28 @@ def convert(path, ms_window=20):
         if len(midi_files) == 0:
             raise FileNotFoundError('No midi files found!')
 
+    windows_per_sec = (1000 / ms_window)
     for midi_file in midi_files:
-        midi = pretty_midi.PrettyMIDI(midi_file)
-        logging.debug('Converting file \'' + midi_file + '\'...')
-        midi_data = midi.instruments[0], midi.instruments[1]
+        npy_file = midi_file + '_' + str(ms_window) + 'ms' + '.npy'
 
-        # Generate empty numpy arrays
-        array_dim = math.ceil(midi.get_end_time() * ms_window)
-        hands = np.zeros((88, array_dim, 2), dtype=np.bool)
+        if overwrite or not os.path.exists(npy_file):
+            midi = pretty_midi.PrettyMIDI(midi_file)
+            logging.debug('Converting file \'' + midi_file + '\'...')
+            midi_data = midi.instruments[0], midi.instruments[1]
 
-        # Fill arrays with data
-        for hand, midi_hand in enumerate(midi_data):
-            for note in midi_hand.notes:
-                start = int(math.floor(note.start * ms_window))
-                end = int(math.ceil(note.end * ms_window))
-                for width in range(start, end):
-                    hands[note.pitch - 21, width, hand] = True
+            # Generate empty numpy arrays
+            array_dim = math.ceil(midi.get_end_time() * windows_per_sec)
+            hands = np.zeros((88, array_dim, 2), dtype=np.bool)
 
-        np.save(midi_file + '.npy', hands)
+            # Fill arrays with data
+            for hand, midi_hand in enumerate(midi_data):
+                for note in midi_hand.notes:
+                    start = int(math.floor(note.start * windows_per_sec))
+                    end = int(math.ceil(note.end * windows_per_sec))
+                    for width in range(start, end):
+                        hands[note.pitch - 21, width, hand] = True
+
+            np.save(npy_file, hands)
 
 
 class Dataset:
@@ -55,36 +59,34 @@ class Dataset:
             if len(npy_files) == 0:
                 raise FileNotFoundError('No numpy arrays found!')
 
-        self._data = []
-        for npy_file in npy_files:
-            self._data.append(np.load(npy_file))
+        self.data = np.concatenate([np.load(npy_file) for npy_file in npy_files], axis=1)
 
     def next_batch(self, n_samples, n_past_windows=0):
         hands = np.zeros((88, n_past_windows + 1, 2, n_samples), dtype=np.bool)
 
         for sample in range(n_samples):
-            file = random.choice(self._data)
-            window = random.randrange(file.shape[1] + n_past_windows)
+            window = random.randrange(self.data.shape[1] + n_past_windows)
 
-            if window >= file.shape[1]:
+            if window >= self.data.shape[1]:
                 data = np.concatenate([
-                    file[:, window - n_past_windows:, :],
-                    np.zeros((88, window - file.shape[1] + 1, 2), dtype=np.bool)
+                    self.data[:, window - n_past_windows:, :],
+                    np.zeros((88, window - self.data.shape[1] + 1, 2), dtype=np.bool)
                 ], axis=1)
             elif window < n_past_windows:
                 data = np.concatenate([
                     np.zeros((88, n_past_windows - window, 2), dtype=np.bool),
-                    file[:, 0:window + 1, :]
+                    self.data[:, 0:window + 1, :]
                 ], axis=1)
             else:
-                data = file[:, window - n_past_windows:window + 1, :]
+                data = self.data[:, window - n_past_windows:window + 1, :]
 
             hands[:, :, :, sample] = data
 
             # import matplotlib.pyplot as plt
             # tmp = (data[:, :, 0].astype(np.int8) - data[:, :, 1].astype(np.int8))
-            #
             # plt.imshow(tmp, cmap='bwr', origin='lower', vmin=-1, vmax=1)
+            # mng = plt.get_current_fig_manager()
+            # mng.window.showMaximized()
             # plt.show()
 
         hands_88 = np.logical_or(hands[:, :, 0, :], hands[:, :, 1, :]).astype(np.int8)
@@ -94,7 +96,7 @@ class Dataset:
 
 
 if __name__ == '__main__':
-    # convert(path='data', ms_window=20)
+    convert(path='data', ms_window=20, overwrite=False)
     foo = Dataset('data')
     for i in range(100):
-        foo.next_batch(400, n_past_windows=0)
+        foo.next_batch(400, n_past_windows=100)
