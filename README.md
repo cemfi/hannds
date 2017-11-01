838e9b71fba4993c45c54416f032063ed5ab6a3a
Aristotelis Hadjakos <thadjakos@gmail.com>
2017-11-01 15:14:16 +0100

    Tweaked learning rate. Added histogram summaries.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 - Installation with Anaconda: Open the shell and go to the main directory of repository. Execute "conda env update --file environment.yml". 
 - Run train_nn.py
 - Output: tensorboard -logdir logs
+
+# TODO
+- Verify MIDI data
+- Separate test and training data
--- train_nn.py
+++ train_nn.py
@@ -16,6 +16,8 @@ if sys.platform == 'darwin':
 
 # logging.basicConfig(level=logging.DEBUG)
 
+TRAINING_STEPS = 10000
+BATCH_SIZE = 100
 LOG_PATH = os.path.join('logs', datetime.datetime.now().strftime('%H-%M'))
 
 
@@ -60,15 +62,20 @@ with tf.name_scope('preprocessing'):
     y_01 = (y_non_nans + 1.0) / 2.0 # needed for cross-entropy calculation
 
 with tf.name_scope('nn'):
-    b_1 = tf.Variable(tf.zeros([88]))
-    W_1 = tf.Variable(tf.zeros([(PAST_WINDOWS + 1) * 88, 88]))
-    h_last = tf.matmul(x_no_nans, W_1) + b_1
+    b_last = tf.Variable(tf.concat([-1.0 * tf.ones([44]), +1.0 * tf.ones([44])], axis=0))
+    W_last = tf.Variable(tf.truncated_normal([(PAST_WINDOWS + 1) * 88, 88], stddev=0.1))
+    pre_last = tf.matmul(x_no_nans, W_last) + b_last
 
     # for squared error loss
     # y_output = tf.tanh(h_last)
 
     # for cross-entropy loss
-    y_output = (tf.sigmoid(h_last) * 2.0) - 1.0
+    y_output = (tf.sigmoid(pre_last) * 2.0) - 1.0
+
+    tf.summary.histogram("b_last", b_last)
+    tf.summary.histogram("W_last", W_last)
+    tf.summary.histogram("pre_last", pre_last)
+    tf.summary.histogram("y_output", y_output)
 
 
 
@@ -88,7 +95,7 @@ with tf.name_scope('optimizer'):
     log_likely = tf.reduce_sum(tf.multiply(y_01, log_prob_right)) + tf.reduce_sum(tf.multiply(1.0 - y_01, log_prob_left))
     loss = -log_likely
 
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+    train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
 # Calculate error rate
 with tf.name_scope('evaluation'):
@@ -100,25 +107,25 @@ with tf.name_scope('evaluation'):
 
 # Summaries
 error_rate_summary = tf.summary.scalar('error_rate', error_rate)
+log_likely_summary = tf.summary.scalar('log_likely', log_likely)
 image_placeholder = tf.placeholder(tf.uint8, fig2rgb_array(get_figure()).shape)
-image_summary = tf.summary.image('output', image_placeholder)
+# image_summary = tf.summary.image('output', image_placeholder)
+merged_summary = tf.summary.merge_all();
 
 with tf.Session() as sess:
-    writer = tf.summary.FileWriter(LOG_PATH, sess.graph)
+    writer = tf.summary.FileWriter(LOG_PATH)
+    writer.add_graph(sess.graph)
     tf.global_variables_initializer().run()
     # Train
-    TRAINING_STEPS = 10000
-    BATCH_SIZE = 100
-
     for i in range(TRAINING_STEPS + 1):
         batch_xs, batch_ys = data.next_batch(BATCH_SIZE)
         batch_xs = batch_xs.reshape([BATCH_SIZE, 88 * (PAST_WINDOWS + 1)])
-        _, error_rate_sum, result, error_rate_value, output_wildcard, any = sess.run([
+        _, merged_sum, result, error_rate_value, output_wildcard, any = sess.run([
             train_step,
-            error_rate_summary,
+            merged_summary,
             masked_predictions,
             error_rate,
-            b_1,
+            b_last,
             y_output
         ], feed_dict={x_in: batch_xs, y_labels: batch_ys})
         if i % 100 == 0:
@@ -132,4 +139,5 @@ with tf.Session() as sess:
         # writer.add_summary(figure_to_summary(fig), i)
 
         # Write other values to summary
-        writer.add_summary(error_rate_sum, i)
+        if i % 50 == 0:
+            writer.add_summary(merged_sum, i)
